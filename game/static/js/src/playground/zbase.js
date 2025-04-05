904d07740dd2320c7d8c7547e6b670dc3432dba5
Json Grigg <json@example.com>
2025-04-05 21:52:54 +0800

    create the game

--- game/static/js/src/playground/zbase.js
+++ game/static/js/src/playground/zbase.js
@@ -1,20 +1,34 @@
 class AcGamePlayground {
     constructor(root) {
         this.root = root;
-        this.$playground = $(`<div>游戏界面</div>`);
+        this.$playground = $(`<div class="ac-game-playground"></div>`);
 
-        this.hide();
-        this.root.$ac_game.append(this.$playground);
-
+        //this.hide();
+        this.show();
         this.start();
     }
 
-    start() {
-        
+    get_random_color() {
+        let colors = ["blue", "green", "grey", "red", "Coral", "Azure" ];
+        return colors[Math.floor(Math.random() * 6)];
     }
 
-    show() {
+    start() {
+    }
+
+    show(){
         this.$playground.show();
+        this.root.$ac_game.append(this.$playground);
+        this.width = this.$playground.width();
+        this.height = this.$playground.height();
+        this.game_map = new GameMap(this);
+        this.players = [];
+        this.players.push(new Player(this, this.width / 2, this.height / 2, this.height * 0.05, "pink", this.height * 0.15, true));
+
+        for (let i = 0 ; i < 6 ; i ++) {
+            this.players.push(new Player(this, this.width / 2, this.height / 2, this.height * 0.05, this.get_random_color(), this.height * 0.15, false));
+        }
+
     }
 
     hide() {
