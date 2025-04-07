c398f21177bbf33a24eb2c529d58d10a5187223c
Json Grigg <json@example.com>
2025-04-07 08:56:49 +0800

    one_click_login on Web

--- static/js/dist/game.js
+++ static/js/dist/game.js
@@ -555,6 +555,8 @@ class Settings {
 
         this.$register.hide();
 
+        this.$acwing_login = this.$settings.find('.ac-game-settings-acwing img');
+
         this.root.$ac_game.append(this.$settings);
 
         this.start();
@@ -566,8 +568,13 @@ class Settings {
     }
 
     add_listening_events() {
+        let outer = this;
         this.add_listening_events_login();
         this.add_listening_events_register();
+
+        this.$acwing_login.click(function(){
+            outer.acwing_login();
+        });
     }
 
     add_listening_events_login() {
@@ -589,6 +596,19 @@ class Settings {
         this.$register_submit.click(function() {
             outer.register_on_remote();
         });
+    }
+
+    acwing_login() {
+        $.ajax({
+            url: "https://app7454.acapp.acwing.com.cn/settings/acwing/web/apply_code/",
+            type: "GET",
+            success: function(resp) {
+                console.log(resp);
+                if (resp.result === "success") {
+                    window.location.replace(resp.apply_code_url);
+                }
+            }
+        })
     }
 
     login_on_remote() {  // 在远程服务器上登录
